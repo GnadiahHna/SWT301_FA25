ff94827cec83a4fed01ae5a2abf8eb5bfa0a1bf0
Đặng Hải Anh <danghaianh10a7@gmail.com>
2025-11-07 12:12:54 +0700

    add exercise

--- Excercise6/src/main/java/danghaianhde190966/example/Main.java
+++ Excercise6/src/main/java/danghaianhde190966/example/Main.java
@@ -1,4 +1,4 @@
-package caoducmanhde190965.example;
+package danghaianhde190966.example;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
